d4481c1bc9374d91c6b4a2add7d50038390998b3
Monchoraptor <amoracho98@gmail.com>
2020-12-30 16:06:23 +0100

    Usuario hecho, con getters y setters. Creado un ArrayList estatico para almacenar los usuarios. Creado  el metodo de guardar la clasificacion tambien

--- src/mastermind/Clasificacion.java
+++ src/mastermind/Clasificacion.java
@@ -1,14 +1,24 @@
-/*
- * To change this license header, choose License Headers in Project Properties.
- * To change this template file, choose Tools | Templates
- * and open the template in the editor.
- */
 package mastermind;
 
-/**
- *
- * @author sigui
- */
 public class Clasificacion {
-    
+	public static volcar_a_txt(File fichero)
+	{
+		FileWriter writer;
+		int i;
+
+		writer = new FileWriter(fichero);
+		if (fichero.createNewFile())
+		{
+			System.out.println("File created: " + fichero.getName());
+		}
+		else
+		{
+			System.out.println("File already exists.");
+		}
+		while (i < Usuario.getLista_usuarios().size())
+		{
+			writer.write(Usuario.getLista_usuarios().sort(new WinsSorter()).get(i).toString());
+			writer.write("\n");
+		}
+	}
 }
