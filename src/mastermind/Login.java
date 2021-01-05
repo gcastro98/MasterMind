ecc7e33338c1b4a2684e13d82b6ab48ca7f41f01
Monchoraptor <amoracho98@gmail.com>
2021-01-05 02:23:34 +0100

    descomentado el codigo

--- src/mastermind/Login.java
+++ src/mastermind/Login.java
@@ -6,7 +6,6 @@ public class Login
 {
 	public static Usuario sign_in(String usuario, String clave)
 	{
-		return (null);/*
 		int i;
 		ArrayList<Usuario> lista_usuarios;
 		
@@ -18,6 +17,6 @@ public class Login
 				return (lista_usuarios.get(i));
 				i++;
 		}
-		return (null);*/
+		return (null);
 	}
 }
